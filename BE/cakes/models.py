b7cded1412da523def0a455a18f24ed6c006b4ef
Sojeong0430 <dlthwjd1629@gmail.com>
2024-08-11 19:16:59 +0900

    feat : cake_custom , massage_custom 모델 추가

--- BE/cakes/models.py
+++ BE/cakes/models.py
@@ -6,3 +6,12 @@ class message (models.Model):
     party = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     message_content = models.TextField()
 
+class cake_custom (models.Model):
+    base = models.CharField(max_length=100)
+    decorations = models.CharField(max_length=100)
+    syrup = models.CharField(max_length=100)
+
+class message_cutom (models.Model):
+    font = models.CharField(max_length=100)
+    BG_image = models.CharField(max_length=100)
+    space = models.CharField(max_length=100)
