7dcc566b03634c145ec3a14bd400d3afb04d1c64
Sojeong0430 <dlthwjd1629@gmail.com>
2024-08-27 19:49:32 +0900

    chore : 친구 추가 api view 수정

--- BE/contacts/models.py
+++ BE/contacts/models.py
@@ -9,3 +9,7 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.username
+
+#https://도메인/partyroom/<username> << 주인(A) 입장에서의 파티룸 주소
+#https://도메인/partyroom/<username>/visit << 방문자 입장에서의 A의 파티룸 주소
+#http://localhost:3000
